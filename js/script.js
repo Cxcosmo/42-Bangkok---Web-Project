95163c4d7eb519385590a6d94faf99b5d87b5cc3
/ᐠ. ｡.ᐟ\ᵐᵉᵒʷˎˊ˗ <thanapon9114@gmail.com>
2025-09-12 02:05:09 +0700

    feat: User_card_cv

--- js/script.js
+++ js/script.js
@@ -1,10 +1,4 @@
-window.addEventListener("load", function () {
-  const preloader = document.getElementById("preloader");
-  const content = document.getElementById("content");
-
-  // รอ 2.5 วิแล้วค่อย fade out
-  setTimeout(() => {
-    preloader.style.display = "none";
-    content.style.display = "block";
-  }, 2500);
-});
+lucide.createIcons();
+function toggleFlip(card_id){
+    document.getElementById(card_id).classList.toggle('flipped');
+}
